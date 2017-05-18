90183a5ecfa7046000d00048797380772216c452
Jonathan Woolf <jwool003@ucr.edu>
2017-05-17 17:00:01 -0700

    Finally able to upload

---
+++ command.h
@@ -0,0 +1,47 @@
+#ifndef __COMMAND_CLASS__
+#define __COMMAND_CLASS__
+
+#include "composite.h"
+
+class Command {
+	protected:
+		Base* root;
+
+	public:
+		Command() { };
+		double execute() {
+			return root->evaluate();
+		};
+		Base* get_root() {
+			return root;
+		};
+};
+
+class OpCommand : public Command {
+	//OpCommand Code Here
+	public:
+		OpCommand() {
+			root = new Op(0);
+		}
+		OpCommand(double value) {
+			root = new Op(value);
+		}
+};
+
+class AddCommand : public Command {
+	//AddCommand Code Here
+};
+
+class SubCommand : public Command {
+	//SubCommand Code Here
+};
+
+class MultCommand : public Command {
+	//MultCommand Code Here
+};
+
+class SqrCommand : public Command {
+	//SqrCommand Code Here
+};
+
+#endif //__COMMAND_CLASS__
